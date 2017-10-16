eaee35e760f8518f8503734f09c857f49b22e382
albs_br <albs_br@yahoo.com.br>
2017-10-16 18:03:40 -0200

    CS Compiler for JACA in C#, first draf, nothing works at all

---
+++ 2nd gen - 8 bit cpu/CS Compiler/CSCompiler/CSCompiler.Entities/AllClasses.cs
@@ -0,0 +1,82 @@
+﻿using CSCompiler.Entities.MachineCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCompiler.Entities
+{
+    public class Constants
+    {
+        internal const int SIZE_RAM_MEMORY = 32768;
+        internal const int BASE_ADDR_PROGRAM = 0;
+        internal const int BASE_ADDR_SUBROUTINES = 1000;
+        internal const int BASE_ADDR_VARIABLES = 2000;
+    }
+}
+
+namespace CSCompiler.Entities.CS
+{
+    public class CSProgram
+    {
+        public string SourceCodeText;
+
+        public IList<Command> Commands { get; set; }
+
+        public MachineCodeProgram Compile()
+        {
+            var machineCodeProgram = new MachineCodeProgram();
+
+            //string[] breakLineChars = ["a", "b"];
+
+            //var splittedText = this.SourceCodeText.Split()
+
+            for (int i = 0; i < this.SourceCodeText.Length; i++)
+            { 
+                //TODO: continue here
+                char actualChar = SourceCodeText.ElementAt(i);
+
+                if (actualChar == ' ')
+                {
+                    continue;
+                }
+            }
+
+            return machineCodeProgram;
+        }
+    }
+
+    public abstract class Command
+    {
+    }
+
+    public class SimpleCommand : Command
+    { 
+    }
+
+    public class ComplexCommand : Command
+    {
+        IList<Command> InnerCommands { get; set; }
+    }
+}
+
+namespace CSCompiler.Entities.MachineCode
+{ 
+    public class MachineCodeProgram
+    {
+        // Constructor
+        public MachineCodeProgram()
+        {
+            this.bytes = new List<byte>(Constants.SIZE_RAM_MEMORY);
+
+            // Initialize array filled of zeroes
+            for (int i = 0; i < Constants.SIZE_RAM_MEMORY; i++)
+            {
+                this.bytes.Add(0);
+            }
+        }
+
+        public IList<byte> bytes { get; set; }
+    }
+}
---
+++ 2nd gen - 8 bit cpu/CS Compiler/CSCompiler/CSCompiler.Test.Unit/AllTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSCompiler.Entities;
+using CSCompiler.Entities.CS;
+
+namespace CSCompiler.Test.Unit
+{
+    [TestClass]
+    public class AllTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var csSourceCode = "byte myVar = 17;";
+
+            // Act
+            var csProgram = new CSProgram();
+            csProgram.SourceCodeText = csSourceCode;
+            var machineCodeProgram = csProgram.Compile();
+
+            // Assert
+            Assert.AreEqual(32768, machineCodeProgram.bytes.Count);
+            Assert.AreEqual(1, machineCodeProgram.bytes[2000]);
+        }
+    }
+}
